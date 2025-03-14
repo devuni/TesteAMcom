51e5c62e7af7425e7911dee65e11745fcd03ee56
Carlos Eduardo  Magalhães <carloseduardomagalhaes@Carloss-MacBook-Air.local>
2025-03-14 09:34:59 -0300

    Criacao do diretorio Questão 3

--- Questao5/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Questao5/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6aa9e40c10d8434cf2030af4fe660596b44e4de5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9243c372805689f6834bded15fade06166f9a2b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
